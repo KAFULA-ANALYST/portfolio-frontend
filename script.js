b0da5e0413d7c4471c72b3efaf44eaa4a8b3dc64
KWANGU KAFULA <mussalazaro57@gmail.com>
2026-05-16 21:05:48 +0300

    Professional portfolio upgrade

--- script.js
+++ script.js
@@ -1,69 +1,56 @@
-fetch("https://portfolio-backend.onrender.com/api/profile")
-    .then(response => response.json())
-    .then(data => {
+fetch("https://portfolio-backend-8hsl.onrender.com/api/profile")
+.then(response => response.json())
 
-        console.log(data);
+.then(data => {
 
-        // HERO TITLE
-        document.querySelector(".hero p").innerText = data.title;
+    console.log(data);
 
-        // CONTACT SECTION
-        document.querySelector("#contact").innerHTML = `
-            <h2 class="section-title">Contact Me</h2>
+    // SKILLS
 
-            <p>Email: ${data.email}</p>
-            <p>Phone: ${data.phone}</p>
+    const skillsContainer =
+    document.querySelector(".skills-grid");
 
-            <p>
-                GitHub:
-                <a href="${data.social.github}" target="_blank">
-                    ${data.social.github}
-                </a>
-            </p>
+    data.skills.forEach(skill => {
+
+        skillsContainer.innerHTML += `
+        
+        <div class="skill-card">
+
+            <h3>${skill}</h3>
+
+            <p>Loaded from Backend API</p>
+
+        </div>
         `;
-
-        // SKILLS SECTION
-        const skillsContainer = document.querySelector(".skills-grid");
-
-        skillsContainer.innerHTML = "";
-
-        data.skills.forEach(skill => {
-
-            skillsContainer.innerHTML += `
-                <div class="skill-card">
-                    <h3>${skill}</h3>
-                    <p>Skill loaded from Backend API</p>
-                </div>
-            `;
-        });
-
-        // PROJECTS SECTION
-        const projectsContainer =
-            document.querySelector(".projects-grid");
-
-        projectsContainer.innerHTML = "";
-
-        data.projects.forEach(project => {
-
-            projectsContainer.innerHTML += `
-                <div class="project-card">
-
-                    <div class="project-content">
-
-                        <h3>${project.name}</h3>
-
-                        <p>${project.description}</p>
-
-                    </div>
-
-                </div>
-            `;
-        });
-
-    })
-
-    .catch(error => {
-
-        console.error("Error fetching backend API:", error);
-
     });
+
+    // PROJECTS
+
+    const projectsContainer =
+    document.querySelector(".projects-grid");
+
+    data.projects.forEach(project => {
+
+        projectsContainer.innerHTML += `
+        
+        <div class="project-card">
+
+            <div class="project-content">
+
+                <h3>${project.name}</h3>
+
+                <p>${project.description}</p>
+
+            </div>
+
+        </div>
+        `;
+    });
+
+})
+
+.catch(error => {
+
+    console.log(error);
+
+});
